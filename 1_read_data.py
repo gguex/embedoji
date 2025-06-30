09b27251b6504cddab174d0043b14334a23f0fa7
gguex <guillaume.guex@gmail.com>
2025-06-30 12:58:20 +0200

    fine tune the read data script

--- 1_read_data.py
+++ 1_read_data.py
@@ -4,19 +4,12 @@ import emoji
 import polars as pl
 
 DATA_FOLDER = "../swissubase_2579_1_0/data/wns_corpus_v1.0.0/data/plain_text"
-RESULT_FOLDER = "../swissubase_2579_1_0/data/wns_corpus_v1.0.0/data/"
+RESULT_FOLDER = "../swissubase_2579_1_0/data/wns_corpus_v1.0.0/data/corpus_csv"
 
 file_names = os.listdir(DATA_FOLDER)
 file_names.sort()
 
-result_df = pl.Schema({
-    "chat_name": pl.String,
-    "msg_date": pl.String,
-    "msg_user": pl.String,
-    "msg_text": pl.String,
-    "emoji_list": pl.String
-}).to_frame()
-
+msg_id = 1
 for file_name in file_names:
     
     chat_name = file_name.split(".")[0]
@@ -28,12 +21,12 @@ for file_name in file_names:
     chat_res = []
     for line in lines:
 
-        user_pos = re.search(r"_WNS_USER_(\d){3}_", line)
+        user_pos = re.search(r"\d{2}:\d{2} _WNS_USER_(\d){3}_:", line)
         hour_pos = re.search(r"\d{2}:\d{2}", line)
         
         if user_pos:
-            msg_date = line[:user_pos.span()[0]].strip()
-            msg_user = line[user_pos.span()[0]:user_pos.span()[1]].strip()
+            msg_date = line[:(user_pos.span()[0]+6)].strip()
+            msg_user = line[(user_pos.span()[0]+6):(user_pos.span()[1]-1)].strip()
             msg_text = line[user_pos.span()[1]+1:].strip()
         elif hour_pos:
             msg_date = line[:hour_pos.span()[1]].strip()
@@ -50,6 +43,7 @@ for file_name in file_names:
         
         # Fill the DataFrame
         chat_res.append({
+            "msg_id": msg_id,
             "chat_name": chat_name,
             "msg_date": msg_date,
             "msg_user": msg_user,
@@ -57,8 +51,11 @@ for file_name in file_names:
             "emoji_list": emoji_list
         })
         
+        # Increment message ID
+        msg_id += 1
+        
     # Add to results
-    result_df = pl.concat([result_df, pl.DataFrame(chat_res)])
+    result_df = pl.DataFrame(chat_res)
     
-# Save the DataFrame to a Parquet file
-result_df.write_csv(os.path.join(RESULT_FOLDER, "wns_corpus.csv"))
+    # Save the DataFrame to a Parquet file
+    result_df.write_csv(os.path.join(RESULT_FOLDER, f"{chat_name}.csv"))
